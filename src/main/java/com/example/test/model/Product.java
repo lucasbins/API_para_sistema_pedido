b5e7049a22f031b898a2982d795eea4a718a2aa6
lucasbins <lucas.braga@rede.ulbra.br>
2023-09-05 21:06:55 -0300

    feat: create entity class Product

---
+++ src/main/java/com/example/test/model/Product.java
@@ -0,0 +1,23 @@
+package com.example.test.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Float priceInCents;
+    private String description;
+}
