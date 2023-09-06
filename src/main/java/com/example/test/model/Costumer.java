f38b714cb649b7ef84c3734ab343d3f5b23adb31
lucasbins <lucas.braga@rede.ulbra.br>
2023-09-05 21:06:33 -0300

    feat: create entity class Costumer

---
+++ src/main/java/com/example/test/model/Costumer.java
@@ -0,0 +1,20 @@
+package com.example.test.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Costumer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+}
