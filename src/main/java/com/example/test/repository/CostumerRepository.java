b7d97691832a838a909f836842af0ffead070848
lucasbins <lucas.braga@rede.ulbra.br>
2023-09-06 19:54:16 -0300

    feat: create repositories

---
+++ src/main/java/com/example/test/repository/CostumerRepository.java
@@ -0,0 +1,8 @@
+package com.example.test.repository;
+
+import com.example.test.model.Costumer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CostumerRepository extends JpaRepository<Costumer,String>{
+
+}
